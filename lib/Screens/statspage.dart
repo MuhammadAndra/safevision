3a087c9103dbdb05a831168e94f844076781ffc5
MuhammadAndra <m.andradzaki@gmail.com>
2024-10-10 08:56:34 +0700

    [edit] edit connected device and statspage card

--- lib/Screens/statspage.dart
+++ lib/Screens/statspage.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:safevision/Widgets/ActivityChart.dart';
 import 'package:safevision/Widgets/AppBarWidget.dart';
 
-List<String> list = <String>['Last 25 days', 'This month', 'Last 6 month', 'All time'];
+List<String> list = <String>[
+  'Last 25 days',
+  'This month',
+  'Last 6 month',
+  'All time'
+];
 
 class Statspage extends StatelessWidget {
   final List<String> recordedName = [
@@ -95,36 +100,43 @@ class StatsTile extends StatelessWidget {
       ),
       elevation: 0,
       color: usedColor,
-      child: InkWell(
-        onTap: () {
-          // Handle tap action
-        },
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 3,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(22),
+        child: Material(
+          color: usedColor,
+          elevation: 0,
+          child: InkWell(
+            onTap: () {
+              // Handle tap action
+            },
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    recordedName,
+                    style: TextStyle(
+                      // fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    recordedCount.toString(),
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                recordedName,
-                style: TextStyle(
-                  // fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Text(
-                recordedCount.toString(),
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 24,
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/Widgets/ConnectedDevice.dart
+++ lib/Widgets/ConnectedDevice.dart
@@ -43,43 +43,45 @@ class DeviceTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Color usedColor = Theme.of(context).colorScheme.surfaceContainerHigh;
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(22),
-      ),
-      elevation: 0,
-      color: usedColor,
-      child: InkWell(
-        onTap: () {
-          // Handle tap action
-        },
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(
-                device.icon,
-                size: 30,
-                color: Colors.black,
-              ),
-              SizedBox(height: 3,),
-              Text(
-                device.title,
-                style: TextStyle(
-                  // fontWeight: FontWeight.bold,
-                  fontSize: 16,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(22),
+      child: Material(
+        color: usedColor,
+        elevation: 0,
+        child: InkWell(
+          onTap: () {
+            // Handle tap action
+          },
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  device.icon,
+                  size: 30,
+                  color: Colors.black,
                 ),
-              ),
-              Text(
-                device.subtitle,
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 12,
+                SizedBox(
+                  height: 3,
                 ),
-              ),
-            ],
+                Text(
+                  device.title,
+                  style: TextStyle(
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  device.subtitle,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
