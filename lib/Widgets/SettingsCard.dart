dec32a343f1f7316518c0d4c46a3e36cd639ac03
MuhammadAndra <m.andradzaki@gmail.com>
2024-10-11 10:35:08 +0700

    [fix] SettingsCard highlight

--- lib/Widgets/SettingsCard.dart
+++ lib/Widgets/SettingsCard.dart
@@ -4,41 +4,85 @@ class Settingscard extends StatelessWidget {
   final IconData icon;
   final String menuName;
   final Function()? onTap;
-  const Settingscard({super.key, required this.icon, required this.menuName,required this.onTap});
+  const Settingscard(
+      {super.key,
+      required this.icon,
+      required this.menuName,
+      required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-            color: Color(0XFFE2EDF2),
-            border: Border.all(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Material(
+        color: Color(0XFFE2EDF2),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            side: BorderSide(
               color: Color(0xffA2C2D0),
+            )),
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  icon,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: Text(
+                  menuName,
+                  style: TextStyle(fontSize: 18),
+                )),
+                Icon(
+                  Icons.arrow_right,
+                  size: 30,
+                ),
+              ],
             ),
-            borderRadius: BorderRadius.all(Radius.circular(12))),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(
-                icon,
-                size: 30,
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                  child: Text(
-                menuName,
-                style: TextStyle(fontSize: 18),
-              )),
-              Icon(
-                Icons.arrow_right,
-                size: 30,
-              ),
-            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  inkwelled() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Material(
+        color: Color(0XFFE2EDF2),
+        shape: Border.all(color: Color(0xffA2C2D0), style: BorderStyle.solid),
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  icon,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: Text(
+                  menuName,
+                  style: TextStyle(fontSize: 18),
+                )),
+                Icon(
+                  Icons.arrow_right,
+                  size: 30,
+                ),
+              ],
+            ),
           ),
         ),
       ),
